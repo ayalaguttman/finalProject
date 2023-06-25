9613c45c051ec67d9bb888e2f459f29b5a9685f4
ayalaguttman <82307669+ayalaguttman@users.noreply.github.com>
2023-06-25 13:03:43 +0300

    commit 2

---
+++ models/itemModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const itemSchema = new mongoose.Schema({
+    name: String,
+    info: String,
+    img_url: String,
+    worker_id: String,
+    categories_url: String,
+    price: Number,
+    color: String,
+    size: Number,
+    active: {
+        type: Boolean, default: true
+    }
+})
+
+exports.ItemModel = mongoose.model("items", itemSchema);
+
+exports.validateItem = (_reqBody) => {
+    let joiSchema = Joi.object({
+        name: Joi.string().min(2).max(99).required(),
+        info: Joi.string().min(2).max(500).required(),
+        img_url: Joi.string().min(2).max(200).allow(null, ""),
+        categories_url: Joi.string().min(2).max(99).required(),
+        price: Joi.number().min(2).max(99).required(),
+        color: Joi.string().min(2).max(15).required(),
+        size: Joi.number().min(2).max(99).required(),
+    })
+    return joiSchema.validate(_reqBody);
+}
